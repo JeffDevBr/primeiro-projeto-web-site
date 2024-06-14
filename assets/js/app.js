32f4f75c40c58cc3535057b7c6626f4af34cae9b
Jefferson <jeff.dev.br@gmail.com>
2024-06-14 15:14:43 -0300

    trabalhando no menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,45 @@
-const loadHTML = async (e, url) => {
+async function loadHTML(elementId, url) {
     const response = await fetch(url)
     const text = await response.text()
-    const pagina = document.getElementById(e)
-    pagina.innerHTML = text
+    document.getElementById(elementId).innerHTML = text
 }
 
-loadHTML('header', 'header.html');
-loadHTML('footer', 'footer.html');    
+document.addEventListener('DOMContentLoaded', async () => {
+    await loadHTML('header', 'header.html')
+    await loadHTML('footer', 'footer.html')
+
+    menu()
+})
+
+
+function menu() {
+    const drawer = document.querySelector('#drawer')
+    const open = document.querySelector('[data-drawer="open"]')
+    const close = document.querySelector('[data-drawer="close"]')
+    const toogle = document.querySelector('[data-drawer="toogle"]')
+
+    const openMenu = () => {
+        if (drawer)
+            drawer.classList.add('opened')
+    }
+
+    const closeMenu = () => {
+        if (drawer)
+            drawer.classList.remove('opened')
+    }
+
+    const toggleMenu = () => {
+        if (drawer)
+            drawer.classList.toggle('opened')
+    }
+
+    open.addEventListener('click', openMenu)
+    close.addEventListener('click', closeMenu)
+    toogle.addEventListener('click', toggleMenu)
+
+}
+
+
+
+
+
